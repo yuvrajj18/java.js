02b78e4c1e8baf8e67df77225966405f71971bb2
yuvrajj18 <159643167+yuvrajj18@users.noreply.github.com>
2024-10-28 13:31:06 +0530

    conversions of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33abcd"
+console.log (typeof score);
+
+let numberinSCORE = Number(score);
+console.log(typeof numberinSCORE);
+console.log(numberinSCORE) //NaN => NOT A NUMBER 
+
+
+let Score = null
+let numberinscore = Number(Score);
+console.log(numberinscore);
+
+
+let SCore = true
+let Numberinscore = Number(SCore);
+console.log(Numberinscore);
+
+// "33" = 33
+//"33abcd" = NaN
+// true = 1 ; false = 0 
+
+let isloggedin = "uv"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+
+// 1= true ; 0 = false
+// "" = false
+//"yuvraj" = true
+
+let someNumberbe = 33
+let stringNumber = String(someNumberbe)
+console.log(typeof stringNumber);
+
